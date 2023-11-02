021fc378f4041fe11300d57b741e90c704d9d729
Grzegorz Aleksander Klementowski <125459323+Klemens-Gak@users.noreply.github.com>
2023-11-02 14:34:34 +0100

    Add files via upload

--- Java Script/script.js
+++ Java Script/script.js
@@ -19,3 +19,4 @@ function changeText()
     document.getElementById("text").innerHTML="<b>Ogólnie</b></br></br>Jeśli szukasz produktów IT dla swojego biznesu to trafiłeś idealnie.Chcesz rozwinąć firmę, dostać o niej precyzyjne dane? Wystarczy, że domnie napiszesz.Możesz zamówić profesjonalną stronę internetową w <u>dobrej cenie</u>oraz  otrzymać wykresy prawdopodobnych zysków lub strat na przyszły miesiąc. Przyjmuję także zamówienia na modele imitujące ekonomię  biznesu i sprawdzeniewpływu choćby najmniejszych zmian na twoją firmę. <b>Wystarczy, że do mnie napiszesz!</b>";
 }
 window.onload=changeText;
+
